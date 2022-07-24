3c4c2a4f044b2ba0929bd92fdde6134f8785b308
Onion323 <70784031+Onion323@users.noreply.github.com>
2022-07-24 16:02:14 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDSzZElhySLB8dmI9gfmtwJzOFTUqmPw5c",
+  authDomain: "storytelling-app-b88c6.firebaseapp.com",
+  projectId: "storytelling-app-b88c6",
+  storageBucket: "storytelling-app-b88c6.appspot.com",
+  messagingSenderId: "918471172413",
+  appId: "1:918471172413:web:c0eb429da78cba267ce554"
+};
